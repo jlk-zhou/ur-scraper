55a64d6416dd38ec5b23e887c0f523864ef52729
Kevin Joe <jlk.zhou@gmail.com>
2025-08-10 18:26:12 +0800

    Added: print vacancies for each ward

--- scraper.py
+++ scraper.py
@@ -3,12 +3,28 @@ have UR rental property vacancies available, and how many. """
 
 from bs4 import BeautifulSoup
 from re import compile
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
 from urllib.request import urlopen
 
-html = urlopen("https://www.ur-net.go.jp/chintai/kanto/tokyo/area/")
-bs = BeautifulSoup(html, 'html.parser')
+# Initialize Selenium to crawl vacancy data
+options = Options()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.ur-net.go.jp/chintai/kanto/tokyo/area/")
 
-zones = bs.find_all("label", 
-                    {"for": compile("^area_0[1-5]$")})
-for zone in zones: 
-    print(zone.get_text().strip())
+pageSource = driver.page_source
+bs = BeautifulSoup(pageSource, 'html.parser')
+
+# Find wards element
+wards = bs.find_all("li", 
+                    {"class": "item_list js-searchMain", 
+                     "data-count-key": compile("^1\d\d$")})
+
+# Print out data
+for ward in wards: 
+    ward_name = ward.find("em", 
+                          {"class": "js-label-text"}).get_text()
+    ward_num = ward.find("span", 
+                         {"class": "item_count js-searchMain-count"}).get_text()
+    print(f"{ward_name}, {ward_num}")
